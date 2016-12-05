d2f40fa370526af4f5b8286f1860faaacac49c00
Chunyu Zhao <zhaocy.dut@gmail.com>
2016-12-05 12:56:37 -0500

    Update assigner.py

--- dnabclib/assigner.py
+++ dnabclib/assigner.py
@@ -2,9 +2,9 @@ import itertools
 
 
 class BarcodeAssigner(object):
-    def __init__(self, samples, mismatches=0, revcomp=True):
+    def __init__(self, samples, mismatches=1, revcomp=True):
         self.samples = samples
-        if mismatches not in [0]:
+        if mismatches not in [0,1,2]:
             raise ValueError(
                 "Only 0 mismatches allowed (got %s)" % mismatches)
         self.mismatches = mismatches
